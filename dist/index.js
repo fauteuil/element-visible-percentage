ee0a0f7b7c80025927163bf20a00cf3ec3c2362c
DHeld000 <dave.held@comcast.com>
2020-11-09 09:39:41 -0800

    chore(publishing): fix publishing module issues

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,12 @@
  * Get the percentage of the height of an Element currently within the viewport.
  * @param {Element | null} element
  */
-const getPercentVisible = (element) => {
+var getPercentVisible = function (element) {
     if (element === null) {
         return 0;
     }
-    const { top, bottom, height } = element.getBoundingClientRect();
-    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    var _a = element.getBoundingClientRect(), top = _a.top, bottom = _a.bottom, height = _a.height;
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
     return top > viewportHeight
         ? 0
         : (Math.min(viewportHeight, bottom) - Math.max(0, top)) / height;
